99e8cc43647370cb5a9c06ef06ea36732b7eb043
makarandapathirana <makarandapathirana2@gmail.com>
2024-06-16 23:53:41 +0530

    edited tasks table

--- database/migrations/2024_06_16_175416_create_tasks_table.php
+++ database/migrations/2024_06_16_175416_create_tasks_table.php
@@ -12,6 +12,7 @@ return new class() extends Migration {
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+
             $table->timestamps();
         });
     }
